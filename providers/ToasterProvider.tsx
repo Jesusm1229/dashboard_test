1823ad26c21b6ce11e3ee2e4ec53abebe7f29d20
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-08-22 23:09:50 -0400

    removing provider toaster

--- providers/ToasterProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Toaster } from "react-hot-toast";
-
-const ToasterProvider = () => {
-    return <Toaster toastOptions={{ duration: 4000, position: 'bottom-center' }} />;
-};
-
-export default ToasterProvider;
