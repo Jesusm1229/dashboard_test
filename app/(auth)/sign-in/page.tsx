693b8f62606cded6b6f707b7e5e8138d2e95d219
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-08-22 23:24:04 -0400

    suspense wrapper

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -14,26 +14,28 @@ export default function Page() {
     );
 
     return (
-        <Suspense>
-            <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
-                <div
-                    className="flex-col items-center justify-center h-full px-4 lg:flex" >
-                    <div className="pt-16 space-y-4 text-center">
-                        <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
-                        <p className="text-base text-[#7E8CA0]">
-                            Log in or Create account to get to your dashboard!
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center mt-8">
-                        <div className="flex flex-col mt-6 mb-6 pointer-events-auto">
-                            {preferredSignInOption}
-                        </div>
-                    </div>
+
+        <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+            <div
+                className="flex-col items-center justify-center h-full px-4 lg:flex" >
+                <div className="pt-16 space-y-4 text-center">
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+                    <p className="text-base text-[#7E8CA0]">
+                        Log in or Create account to get to your dashboard!
+                    </p>
                 </div>
-                <div className="items-center justify-center hidden h-full bg-blue-700 lg:flex">
-                    <Image src="/logo.svg" height={100} width={100} alt="logo" />
+                <div className="flex items-center justify-center mt-8">
+                    <div className="flex flex-col mt-6 mb-6 pointer-events-auto">
+                        <Suspense>
+                            {preferredSignInOption}
+                        </Suspense>
+                    </div>
                 </div>
             </div>
-        </Suspense>
+            <div className="items-center justify-center hidden h-full bg-blue-700 lg:flex">
+                <Image src="/logo.svg" height={100} width={100} alt="logo" />
+            </div>
+        </div>
+
     );
 }
